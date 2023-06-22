4ef9518de1ae14e452de0210a05a8c61ad59c71d
Kiran <75929997+Kiran1689@users.noreply.github.com>
2023-06-22 20:50:51 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
 import Home from "./components/Home";
 import Resume from "./components/Resume";
 
 const App = () => {
-	const [result, setResult] = useState({});
+  const [result, setResult] = useState({});
 
-	return (
-		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<Home setResult={setResult} />} />
-					<Route path='/resume' element={<Resume result={result} />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/home" element={<Home setResult={setResult} />} />
+          <Route path="/resume" element={<Resume result={result} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
