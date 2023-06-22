b93fd1a24983ed85296ad16419fe79fe7088a834
Kiran <75929997+Kiran1689@users.noreply.github.com>
2023-06-22 20:48:09 +0530

    Update Loading.js

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "../loading.css"; // Importing the CSS file for Loading component
 
 const Loading = () => {
-	return (
-		<div className='app'>
-			<h1>Loading, please wait...</h1>
-		</div>
-	);
+  return (
+    <div className="loading-page">
+      <div className="background-image"></div>
+      <h1>Building, please wait...</h1>
+    </div>
+  );
 };
 
 export default Loading;
